c97e92d14bd2a9b5a9022745f0952822696c65b9
Vitalik <mogolivskiy@gmail.com>
2015-02-18 12:31:39 +0200

    initialize repository

---
+++ src/main/java/com/google/training/helloworld/Constants.java
@@ -0,0 +1,15 @@
+package com.google.training.helloworld;
+
+import com.google.api.server.spi.Constant;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming your API.
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "test2spot";
+  public static final String ANDROID_CLIENT_ID = "test2spot";
+  public static final String IOS_CLIENT_ID = "test2spot";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+  public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
---
+++ src/main/java/com/google/training/helloworld/HelloClass.java
@@ -0,0 +1,23 @@
+package com.google.training.helloworld;
+
+public class HelloClass {
+    public String message = "Hello World";
+
+    public HelloClass () {
+    }
+
+    public HelloClass (String name) {
+        this.message = "Hello " + name + "!";
+    }
+    public HelloClass (String name, String period) {
+        this.message = "Good "+period+" " + name + "!";
+        System.out.println("2");
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    
+    
+
+}
